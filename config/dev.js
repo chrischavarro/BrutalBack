bad8589054cfae1c745cd2877f652804c0b8ecb4
Christian Chavarro <christian@clutchprep.com>
2018-01-23 22:53:56 -0600

    Removed unnecessary data

--- config/dev.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  googleClientID: '473560978936-bd32dfngffbv9lba0fm3st9g0r1l9i96.apps.googleusercontent.com',
-  googleClientSecret: '3tRR1in6JOiSKMHqI1YWVnFo',
-  mongoURI: 'mongodb://chrischavarro:Nintendods92@ds111258.mlab.com:11258/brutal-back-dev',
-  cookieKey: 'GDETWERW'
-}
